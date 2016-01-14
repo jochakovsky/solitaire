546bd9f56ee168cef7aeb62bf5739c8edacedd35
Joshua Ochakovsky <jochakovsky@users.noreply.github.com>
2016-01-14 15:37:40 -0500

    Placeholder info displayed

--- app/app.js
+++ app/app.js
@@ -1,1 +1,49 @@
-var app = angular.module('solitare', [ ]);
+(function(){
+    var app = angular.module('solitare', [ ]);
+
+    app.controller('SolitareGameController', function(){
+        this.deck = {
+            active: "4h"
+        };
+
+        this.foundations = [
+            {
+                id: 0
+            },
+            {
+                id: 1
+            },
+            {
+                id: 2
+            },
+            {
+                id: 3
+            }
+        ];
+
+        this.piles = [
+            {
+                id: 0
+            },
+            {
+                id: 1
+            },
+            {
+                id: 2
+            },
+            {
+                id: 3
+            },
+            {
+                id: 4
+            },
+            {
+                id: 5
+            },
+            {
+                id: 6
+            }
+        ];
+
+    });
+})();
