89df5c70f81eeb177624877a9a606966c2ba4013
Joshua Ochakovsky <jochakovsky@users.noreply.github.com>
2016-03-10 18:10:11 -0500

    Gulp is now configured to minify javascript

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
 var gulp = require('gulp');
 
 var eslint = require('gulp-eslint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+var jsPath = 'app/**/*.js';
+var jsTestPath = 'app/**/*Test.js';
+var jsAppPath = [jsPath, '!' + jsTestPath];
+var watchTasks = ['lint', 'lint-test', 'scripts'];
 
 gulp.task('lint', function() {
-    return gulp.src(['app/**/*.js', '!app/**/*Test.js'])
+    return gulp.src(jsAppPath)
         .pipe(eslint({
             "configFile": "eslint/browser.json"
         }))
@@ -12,7 +20,7 @@ gulp.task('lint', function() {
 });
 
 gulp.task('lint-test', function() {
-    return gulp.src(['app/**/*Test.js'])
+    return gulp.src(jsTestPath)
         .pipe(eslint({
             "configFile": "eslint/jasmine.json"
         }))
@@ -20,8 +28,16 @@ gulp.task('lint-test', function() {
         .pipe(eslint.failAfterError());
 });
 
-gulp.task('watch', function() {
-    gulp.watch('app/**/*.js', ['lint', 'lint-test']);
+gulp.task('scripts', function() {
+    return gulp.src(jsAppPath)
+        .pipe(concat('all.js'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('build/'));
 });
 
-gulp.task('default', ['lint', 'lint-test', 'watch']);
+gulp.task('watch', function() {
+    gulp.watch(jsPath, watchTasks);
+});
+
+gulp.task('default', watchTasks.concat(['watch']));
